3516285e0169f47d7874cbe30aa9c14564cdd9ef
Buro26-Henri <henri@buro26.nl>
2021-04-10 18:35:05 +0200

    init

---
+++ app/MyApp/Controller/HelloWorld.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types=1);
+
+
+namespace MyApp\Controller;
+
+use Swift\Controller\AbstractController;
+use Swift\HttpFoundation\JsonResponse;
+use Swift\Router\Attributes\Route;
+use Swift\Router\Types\RouteMethodEnum;
+
+/**
+ * Class HelloWorld
+ * @package MyApp\Controller
+ */
+class HelloWorld extends AbstractController {
+
+    /**
+     * Get started at baseUrl /hello-world/
+     *
+     * @return JsonResponse
+     */
+    #[Route(method: [RouteMethodEnum::GET], route: '/hello-world/', name: 'myapp.helloworld')]
+    public function helloWorld(): JsonResponse {
+        return new JsonResponse(['message' => 'Hey legend! Get started at https://henrivantsant.github.io/swift-docs/']);
+    }
+
+}

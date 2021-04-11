16cb197ec5076188d52b27440001a3f1394d4023
Buro26-Henri <henri@buro26.nl>
2021-04-11 19:52:00 +0200

    init

--- pub/index.php
+++ pub/index.php
@@ -1,5 +1,13 @@
 <?php declare(strict_types=1);
 
+/*
+ * This file is part of the Swift Framework
+ *
+ * (c) Henri van 't Sant <henri@henrivantsant.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 require_once '../vendor/autoload.php';
 
 use Swift\Application\Application;

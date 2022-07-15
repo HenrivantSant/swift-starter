d82c48288207285b823cf0bc7fe92edbd416fdbc
Buro26-Henri <henri@buro26.nl>
2022-07-15 23:19:19 +0200

    update to swift 0.2

--- app/MyApp/Controller/HelloWorld.php
+++ app/MyApp/Controller/HelloWorld.php
@@ -1,12 +1,12 @@
 <?php declare(strict_types=1);
 
 
-namespace MyApp\Controller;
+namespace App\MyApp\Controller;
 
 use Swift\Controller\AbstractController;
 use Swift\HttpFoundation\JsonResponse;
 use Swift\Router\Attributes\Route;
-use Swift\Router\Types\RouteMethodEnum;
+use Swift\Router\Types\RouteMethod;
 
 /**
  * Class HelloWorld
@@ -19,7 +19,7 @@ class HelloWorld extends AbstractController {
      *
      * @return JsonResponse
      */
-    #[Route(method: [RouteMethodEnum::GET], route: '/hello-world/', name: 'myapp.helloworld')]
+    #[Route(method: [RouteMethod::GET], route: '/hello-world/', name: 'myapp.helloworld')]
     public function helloWorld(): JsonResponse {
         return new JsonResponse(['message' => 'Hey legend! Let\'s get started at https://swiftapi.github.io/swift-docs/docs/']);
     }
